7774a5b1d2a16aa1746104f37e29ffef5ebb34ab
Telicia Watson <teli203@aol.com>
2021-01-02 18:58:15 -0600

    fixing errors

--- Main/db/index.js
+++ Main/db/index.js
@@ -295,4 +295,38 @@ deleteRole() {
       });
   });
 };
+};
 
+class Employee { 
+  constructor(first_name, last_name, role_id, manager_id) { // might have to edited
+      this.first_name = first_name;
+      this.last_name = last_name;
+      this.role_id = role_id;
+      this.manager_id = manager_id;
+  }
+
+  // Adding an Employee //
+  addEmployee() { 
+      inquirer.prompt([
+          {
+              name: "first_name",
+              type: "input",
+              message: "First name? "
+          },
+          {
+              name: "last_name",
+              type: "input",
+              message: "Last name? "
+          },
+          { 
+              name: "role_id",
+              type: "input",
+              message: "What role does the employee have?"
+          },
+          { 
+              name: "manager_id",
+              type: "input",
+              message: "Who is the employee's manager?" 
+          }
+
+          
