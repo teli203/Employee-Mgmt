4ebcdce4fed996409c22fffc3c04d3f73ce2c7bf
Telicia Watson <teli203@aol.com>
2021-01-03 00:01:18 -0600

    add spacing for cleaner read

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -8,6 +8,7 @@ VALUES
 
 INSERT INTO role
     (title, salary, department_id)
+
 VALUES
     ('Sales Lead', 100000, 1),
     ('Salesperson', 80000, 1),
@@ -20,6 +21,7 @@ VALUES
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
+    
 VALUES
     ('John', 'Doe', 1, NULL),
     ('Mike', 'Chan', 2, 1),
